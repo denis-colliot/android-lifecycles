d0aa189ebb7b35052f42e39edb6a16387c127b91
Denis Colliot <denis.colliot@gmail.com>
2017-06-23 13:31:32 +0200

    Step 4 completion

--- app/src/main/java/com/example/android/lifecycles/step4/BoundLocationManager.java
+++ app/src/main/java/com/example/android/lifecycles/step4/BoundLocationManager.java
@@ -16,36 +16,41 @@
 
 package com.example.android.lifecycles.step4;
 
+import android.arch.lifecycle.Lifecycle;
+import android.arch.lifecycle.LifecycleObserver;
+import android.arch.lifecycle.LifecycleRegistryOwner;
+import android.arch.lifecycle.OnLifecycleEvent;
 import android.content.Context;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.util.Log;
 
-import android.arch.lifecycle.LifecycleObserver;
-import android.arch.lifecycle.LifecycleRegistryOwner;
-
 
 public class BoundLocationManager {
-    public static void bindLocationListenerIn(LifecycleRegistryOwner lifecycleOwner,
-                                              LocationListener listener, Context context) {
+
+    public static void bindLocationListenerIn(final LifecycleRegistryOwner lifecycleOwner,
+                                              final LocationListener listener, final Context context) {
         new BoundLocationListener(lifecycleOwner, listener, context);
     }
 
     @SuppressWarnings("MissingPermission")
     static class BoundLocationListener implements LifecycleObserver {
         private final Context mContext;
-        private LocationManager mLocationManager;
         private final LocationListener mListener;
+        private LocationManager mLocationManager;
 
-        public BoundLocationListener(LifecycleRegistryOwner lifecycleOwner,
-                                     LocationListener listener, Context context) {
+        public BoundLocationListener(final LifecycleRegistryOwner lifecycleOwner,
+                                     final LocationListener listener, final Context context) {
             mContext = context;
             mListener = listener;
-            //TODO: Add lifecycle observer
+            lifecycleOwner.getLifecycle().addObserver(this);
         }
 
-        //TODO: Call this on resume
+        /**
+         * Method called on resume event.
+         */
+        @OnLifecycleEvent(Lifecycle.Event.ON_RESUME)
         void addLocationListener() {
             // Note: Use the Fused Location Provider from Google Play Services instead.
             // https://developers.google.com/android/reference/com/google/android/gms/location/FusedLocationProviderApi
@@ -56,14 +61,17 @@ public class BoundLocationManager {
             Log.d("BoundLocationMgr", "Listener added");
 
             // Force an update with the last location, if available.
-            Location lastLocation = mLocationManager.getLastKnownLocation(
+            final Location lastLocation = mLocationManager.getLastKnownLocation(
                     LocationManager.GPS_PROVIDER);
             if (lastLocation != null) {
                 mListener.onLocationChanged(lastLocation);
             }
         }
 
-        //TODO: Call this on pause
+        /**
+         * Method called on pause event.
+         */
+        @OnLifecycleEvent(Lifecycle.Event.ON_PAUSE)
         void removeLocationListener() {
             if (mLocationManager == null) {
                 return;
